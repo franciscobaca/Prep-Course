e78903a7e824f9650820df6251a9a0735c6796c4
franciscobaca <panchobaca10@gmail.com>
2022-06-17 12:13:06 -0300

    Se resolvieron ejercicios de JSV

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -8,12 +8,52 @@ function crearUsuario() {
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
+
+  // Metodo Function Constructor
+  
+  function Usuario(opciones){
+    this.usuario = opciones.usuario;
+    this.nombre = opciones.nombre;
+    this.email = opciones.email;
+    this.contraseña = opciones.contraseña;
+  }
+
+  Usuario.prototype.saludar = function(){
+    return "Hola, mi nombre es " + this.nombre;
+  }
+
+  return Usuario;
+
+
+  /* Metodo Class Constructor
+  
+  class Usuario{
+    constructor(opciones){
+      this.usuario = opciones.usuario;
+      this.nombre = opciones.nombre;
+      this.email = opciones.email;
+      this.contraseña = opciones.contraseña];
+      this.saludar = function(){
+        return "Hola, mi nombre es " + this.nombre;
+      }
+    }
+  }
+
+  return Usuario;
+  
+  */
 }
+  
 
 function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
+
+  Constructor.prototype.saludar = function(){
+    return "Hello World!"
+  }
+
 }
 
 function agregarStringInvertida() {
@@ -22,7 +62,20 @@ function agregarStringInvertida() {
   // Ej: 'menem'.reverse() => menem
   // 'toni'.reverse() => 'inot'
   // Pista: Necesitarás usar "this" dentro de "reverse"
+
+  String.prototype.reverse = function(){
+    var stringInvertida = "";
+
+    for (var i = this.length -1; i>=0; i--){
+      stringInvertida = stringInvertida + this[i];
+    }
+  }
+
+  return stringInvertida;
 }
+
+//String = "C a s a"
+//i      =  0 1 2 3
 
 // ---------------------------------------------------------------------------//
   //Crea el constructor de la clase "Persona"
@@ -34,13 +87,6 @@ function agregarStringInvertida() {
     //   Edad: 22,
     //   Domicilio: 'Saavedra 123'
     //  }
-
-  class Persona {
-    constructor(/*Escribir los argumentos que recibe el constructor*/) {
-      // Crea el constructor:
-
-    }
-}
 
 function crearInstanciaPersona(nombre, apellido, edad, dir) {
   //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
